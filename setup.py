3bad6f9bcf030d255908b0b14ba4e74488563d6a
Don Erhan <erhan.andrei.a@gmail.com>
2024-02-28 21:45:57 +0200

    update setup

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="Super_lib",
+    name="Superlib",
     version="0.0.1",
     author="Don Erhan",
     author_email="docker.andrei@gmail.com",
     url="",
     description="Description of lib",
     packages=find_packages(),
+    py_modules=['src'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     install_requires=["tqdm"],
-    entry_points={"console_scripts": ["Super_lib = src.main:main"]},
+    entry_points={"console_scripts": ["Superlib = src.main:main"]},
 )
