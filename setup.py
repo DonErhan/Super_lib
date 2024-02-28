7691acd990ad20e8fac14727e6ac1f50b1910503
Don Erhan <erhan.andrei.a@gmail.com>
2024-02-28 20:47:06 +0200

    req aded

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-
+    install_requires=["tqdm"],
     entry_points={"console_scripts": ["Super_lib = src.main:main"]},
 )
